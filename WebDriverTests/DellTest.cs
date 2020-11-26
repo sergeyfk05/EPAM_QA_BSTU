dbf92fff0305b610812cc72f98f3effaff6c0689
sergeyfk05 <sergeyfk05@gmail.com>
2020-11-27 00:38:36 +0300

    tests

--- WebDriverTests/DellTest.cs
+++ WebDriverTests/DellTest.cs
@@ -8,10 +8,11 @@ using System.Collections.ObjectModel;
 using System.Globalization;
 using WebDriverTests;
 using System.Collections.Generic;
+using WebDriverTests.ElementFilters;
 
 namespace WebDriverTests
 {
-    public class DellTests
+    public class DellTests : IDisposable
     {
         private readonly IWebDriver driver;
         public DellTests()
@@ -19,65 +20,55 @@ namespace WebDriverTests
             driver = new ChromeDriver();
         }
 
-        ~DellTests()
+        public void Dispose()
         {
             driver?.Quit();
         }
 
 
         [Theory]
-        //[InlineData("https://www.dell.com/en-us/shop/cty/pdp/spd/xps-15-9500-laptop/xn9500cto210s")]
-        //[InlineData("https://www.dell.com/en-us/shop/dell-ultrasharp-32-8k-monitor-up3218k/apd/210-alez/monitors-monitor-accessories")]
-        //[InlineData("https://www.dell.com/en-us/shop/dell-laptops/new-dell-g7-17-gaming-laptop/spd/g-series-17-7700-laptop/gn7700ehyyh")]
+        [InlineData("https://www.dell.com/en-us/shop/cty/pdp/spd/xps-15-9500-laptop/xn9500cto210s")]
+        [InlineData("https://www.dell.com/en-us/shop/dell-ultrasharp-32-8k-monitor-up3218k/apd/210-alez/monitors-monitor-accessories")]
+        [InlineData("https://www.dell.com/en-us/shop/dell-laptops/new-dell-g7-17-gaming-laptop/spd/g-series-17-7700-laptop/gn7700ehyyh")]
         [InlineData("https://www.dell.com/en-us/shop/new-alienware-low-profile-rgb-mechanical-gaming-keyboard-aw510k/apd/580-aimo/pc-accessories")]
-        //[InlineData("https://www.dell.com/en-us/shop/desktop-computers/new-inspiron-24-5000-black-all-in-one-with-bipod-stand/spd/inspiron-24-5400-aio/na5400ekphh")]
-        //[InlineData("https://www.dell.com/en-us/shop/makerbot-replicator-3d-printer/apd/a9231730/printers-ink-toner")] 
+        [InlineData("https://www.dell.com/en-us/shop/desktop-computers/new-inspiron-24-5000-black-all-in-one-with-bipod-stand/spd/inspiron-24-5400-aio/na5400ekphh")]
+        [InlineData("https://www.dell.com/en-us/shop/dell-laptops/alienware-m17-r3-gaming-laptop/spd/alienware-m17-r3-laptop/wnm17r312sbf")]
+        [InlineData("https://www.dell.com/en-us/shop/desktop-computers/inspiron-desktop/spd/inspiron-3880-desktop/nd3880eejks")]
+        [InlineData("https://www.dell.com/en-us/shop/dell-urban-backpack-15/apd/460-bbyl/carrying-cases")]
+        [InlineData("https://www.dell.com/en-us/shop/kensington-ld4650p-usb-c-universal-dock-with-k-fob-smart-lock-docking-station-usb-c-gige-north-america/apd/aa659274/pc-accessories")]
+        [InlineData("https://www.dell.com/en-us/shop/dell-32-curved-gaming-monitor-s3220dgf/apd/210-atyt/monitors-monitor-accessories")]
+        [InlineData("https://www.dell.com/en-us/shop/pny-nvidia-quadro-rtx-6000-graphics-card-24-gb-gddr6-pcie-30-x16-4-x-displayport-usb-c/apd/aa413562/graphic-video-cards")]
+        [InlineData("https://www.dell.com/en-us/shop/samsung-98-inch-tv-2020-qled-8k-ultra-hd-hdr10-smart-tv-q900-series-qn98q900rbfxza/apd/ab168598/tv-home-theater")]
         public void AddingToCartTest(string productLink)
         {
             //product page
             driver.Navigate().GoToUrl(productLink);
 
-            IWebElement addToCartButton = driver.SafeFindElementBy(By.XPath("//*[@data-testid='addToCartButton']"));
+            IWebElement addToCartButton = driver.SafeFindElementBy(ProductPageElementFilters.AddToCartButton);
 
             driver.AcceptCookies();            
-            double ProductPageCost = Convert.ToDouble(driver.SafeFindElementBy(new List<By>()
-            {
-                By.XPath("//div[@id='cf-strike-through-price']/div/div[@data-price]"),
-                By.XPath("//*[@data-testid='sharedPSPDellPrice']")
-            }).Text.Replace("$", ""), 
-            Helpers.CostToDoubleConverterProvider);            
+            double ProductPageCost = Convert.ToDouble(driver.SafeFindElementBy(ProductPageElementFilters.PriceText).Text.Replace("$", ""),
+                Helpers.CostToDoubleConverterProvider);            
             
-            string ProductPageItemTitle = driver.SafeFindElementBy(new List<By>()
-            {
-                By.XPath("//h1[@class='cf-pg-title']"),
-                By.XPath("//*[@id='page-title']/div/div/h1/span")
-            }).Text.Trim();
+            string ProductPageItemTitle = driver.SafeFindElementBy(ProductPageElementFilters.ProductTitle).Text.Trim();
 
 
             addToCartButton.Click();
 
-            driver.SafeFindElementBy(By.XPath("//h3[@id='cf-subtotal-label']"));
+            driver.SafeFindElementBy(ProductPageElementFilters.ElementConfirmingAddingToCart);
 
             //cart page
             driver.Navigate().GoToUrl("https://www.dell.com/en-us/buy?cs=19");
-            double CartPageCost = Convert.ToDouble(driver.SafeFindElementBy(new List<By>()
-            {
-                By.XPath("//*[@data-testid='itemssubtotalItemValue']"),
-                By.XPath("//*[@data-testid='cartSubtotal']")
-            }).GetHiddenText(driver).Replace("$", ""),
-            Helpers.CostToDoubleConverterProvider);
+            double CartPageCost = Convert.ToDouble(driver.SafeFindElementBy(CartPageElementFilters.SubtotalCost).GetHiddenText(driver).Replace("$", ""),
+                Helpers.CostToDoubleConverterProvider);
 
-            string CartPageItemTitle = driver.SafeFindElementBy(By.XPath("//*[@data-testid='itemTitle']")).Text.Trim();
-            int CartPageCountItems = Convert.ToInt32(driver.SafeFindElementBy(By.XPath("//select[@name='repeatSelect']/option[@selected='selected']")).Text);
+            string CartPageProductTitle = driver.SafeFindElementBy(CartPageElementFilters.ProductTitle).Text.Trim();
+            int CartPageCountItems = Convert.ToInt32(driver.SafeFindElementBy(CartPageElementFilters.ProductCount).Text);
 
             Assert.Equal(1, CartPageCountItems);
-            Assert.Equal(ProductPageItemTitle, CartPageItemTitle);
             Assert.Equal(ProductPageCost, CartPageCost);
+            Assert.Equal(ProductPageItemTitle, CartPageProductTitle);
         }
-
-
-
-
 
 
     }
---
+++ WebDriverTests/ElementFilters/CartPageElementFilters.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebDriverTests.ElementFilters
+{
+    public static class CartPageElementFilters
+    {
+        public static readonly IEnumerable<By> SubtotalCost = new List<By>()
+            {
+                By.XPath("//*[@data-testid='itemssubtotalItemValue']"),
+                By.XPath("//*[@data-testid='cartSubtotal']")
+            };
+        public static readonly IEnumerable<By> ProductCount = new List<By>()
+            {
+                By.XPath("//select[@name='repeatSelect']/option[@selected='selected']")
+            };
+        public static readonly IEnumerable<By> ProductTitle = new List<By>()
+            {
+                By.XPath("//*[@data-testid='itemTitle']")
+            };
+    }
+}
